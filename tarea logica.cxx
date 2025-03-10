a88179db8fcb76182d8cb5311dd1e2ede5425a4d
am_not_berry <karlaacmora@gmail.com>
2025-03-10 13:27:02 -0600

    Add files via upload

---
+++ tarea logica.cxx
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+  int main() 
+  { 
+  // Calcular la expresión 
+  double resultado =((1.0/2.0) * 3.0 + ((3.0 + 3.0)/2.0) * (2.0 * 3.0 * 3.0)) / ((2.0 + 3.0) * 3.0); 
+  
+  // Imprimir el resultado
+  
+    printf("El resultado es: %f\n", resultado);
+     return 0;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
